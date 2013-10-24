8822dee96841d09689b5751223b32d4eea4d3b26
Freixo <hugo1245@hotmail.com>
2013-10-24 12:14:43 +0100

    Valid_move.pl corrected

--- Valid_move.pl
+++ Valid_move.pl
@@ -2,44 +2,41 @@ even(N) :- N mod 2 =:= 0.
 odd(N) :- \+ even(N).
 
 valid_move(Line, Column, LineTo, Column) :-
-	LineTo = Line + 1.
+	LineTo is Line + 1.
 
 valid_move(Line, Column, LineTo, Column) :-
-	LineTo = Line - 1.
+	LineTo is Line - 1.
 	
 valid_move(Line, Column, Line, ColumnTo) :-
 	ColumnTo = Column + 1.
 	
 valid_move(Line, Column, Line, ColumnTo) :-
-	ColumnTo = Column - 1.	
+	ColumnTo is Column - 1.	
 
 %% Even Columns %%
 
 valid_move(Line, Column, LineTo, ColumnTo) :-
 	even(Column),
-	LineTo = Line + 1,
-	ColumnTo = Column + 1.
+	LineTo is Line + 1,
+	ColumnTo is Column + 1.
 
 valid_move(Line, Column, LineTo, ColumnTo) :-
 	even(Column),
-	LineTo = Line + 1,
-	ColumnTo = Column - 1.
+	LineTo is Line + 1,
+	ColumnTo is Column - 1.
 
 %%				%%
 
+%% Odd Columns %%
+
 valid_move(Line, Column, LineTo, ColumnTo) :-
 	odd(Column),
-	LineTo = Line - 1,
-	ColumnTo = Column + 1.
+	LineTo is Line - 1,
+	ColumnTo is Column + 1.
 	
 valid_move(Line, Column, LineTo, ColumnTo) :-
 	odd(Column),
-	LineTo = Line - 1,
-	ColumnTo = Column - 1.
-
-%% Odd Columns %%
-
-.
-
+	LineTo is Line - 1,
+	ColumnTo is Column - 1.
 
 %%			   %%
