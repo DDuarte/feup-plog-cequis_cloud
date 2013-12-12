1c860d7b4dd9c5f5488e5bcdf4c067e39aadc01c
Freixo <hugo1245@hotmail.com>
2013-12-12 12:11:04 +0000

    Predicate buy added

--- tp2.pl
+++ tp2.pl
@@ -13,3 +13,46 @@ read_file(Stream, [X|L]) :-
     read(Stream, X),
     assert(X),
     read_file(Stream, L).
+    
+buy(disk, CostIn, DiskIn, CostOut, DiskOut) :-
+    disk_unit(U),
+    disk_price(P),
+    CostOut is CostIn + P,
+    DiskOut is DiskIn + U.
+
+buy(ram, CostIn, RAMIn, CostOut, RAMOut) :-
+    ram_unit(U),
+    ram_price(P),
+    CostOut is CostIn + P,
+    RAMOut is RAMIn + U.
+    
+buy(cpu, CostIn, CPUIn, CostOut, CPUOut) :-
+    cpu_unit(U),
+    cpu_price(P),
+    CostOut is CostIn + P,
+    CPUOut is CPUIn + U.
+    
+buy(disk, CostIn, DiskIn, CostOut, DiskOut, Q) :-
+    disk_unit(U),
+    disk_price(P),
+    CostOut is CostIn + (P * Q),
+    DiskOut is DiskIn + (U * Q).
+    
+buy(ram, CostIn, RAMIn, CostOut, RAMOut, Q) :-
+    ram_unit(U),
+    ram_price(P),
+    CostOut is CostIn + (P * Q),
+    RAMOut is RAMIn + (U * Q).
+    
+buy(cpu, CostIn, CPUIn, CostOut, CPUOut, Q) :-
+    cpu_unit(U),
+    cpu_price(P),
+    CostOut is CostIn + (P * Q),
+    CPU is CPUIn + (U * Q).
+ 
+init :-
+    read_config,
+    buy(ram, 0, 0, Cost, RAM, 3),
+    write(Cost),
+    nl,
+    write(RAM). 
